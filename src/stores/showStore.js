6d340191203254d847a0c512af7d6a440d7e61fc
Mikeyjpk <michaeljpkerr@gmail.com>
2023-03-30 13:26:08 +1100

    Coin Tracker React.js tutorial build educational

---
+++ src/stores/showStore.js
@@ -0,0 +1,38 @@
+import { create } from "zustand";
+import axios from "axios";
+
+const showStore = create((set) => ({
+  graphData: [],
+  data: null,
+
+  reset: () => {
+    set({ graphData: [], data: null });
+  },
+
+  fetchData: async (id) => {
+    const [graphRes, dataRes] = await Promise.all([
+      axios.get(
+        `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=aud&days=121`
+      ),
+      axios.get(
+        `https://api.coingecko.com/api/v3/coins/${id}?localization=false&market_data=true`
+      ),
+    ]);
+
+    const graphData = graphRes.data.prices.map((price) => {
+      const [timestamp, p] = price;
+      const date = new Date(timestamp).toLocaleDateString("en-au");
+      return {
+        Date: date,
+        Price: p,
+      };
+    });
+
+    set({
+      graphData,
+      data: dataRes.data,
+    });
+  },
+}));
+
+export default showStore;
